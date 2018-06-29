ffa6fcd49be1cee6a7f910123b29ce65477cb57e
ben whalley <ben247@gmail.com>
2018-06-29 11:15:38 +0100

    getJSON used to query api

--- app/script.js
+++ app/script.js
@@ -1,7 +1,19 @@
 // $.getJSON is a shorthand Ajax function
-$.getJSON(
-  "https://environment.data.gov.uk/flood-monitoring/id/floods",
-  function(data) {
-    console.log(data);
-  }
-);
+// $.getJSON(
+//   "https://environment.data.gov.uk/flood-monitoring/id/floods",
+//   function(data) {
+//     console.log(data);
+//   }
+// );
+
+var riverSearch = $("#river-search").val();
+var url =
+  "http://environment.data.gov.uk/flood-monitoring/id/stations?riverName=";
+
+$(document).ready(function() {
+  $("#search-button").click(function() {
+    $.getJSON(url + riverSearch, function(data) {
+      console.log(data);
+    });
+  });
+});
